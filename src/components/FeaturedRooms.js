8ad2fce63eabdfd2fa9e652a14dbcac7c6bb2546
Eduardo Imery <ejwimery@gmail.com>
2019-08-09 11:08:43 -0600

    creating room component and loading component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
@@ -7,6 +10,12 @@ export default class FeaturedRooms extends Component {
     const { featuredRooms: rooms } = this.context;
     console.log(rooms);
 
-    return <div>from featured room </div>;
+    return (
+      <div>
+        from featured room
+        <Room />
+        <Loading />
+      </div>
+    );
   }
 }
