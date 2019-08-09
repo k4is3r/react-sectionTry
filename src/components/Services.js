91eb3d2b6bb6ca0f154b08f40375f827894c587c
Eduardo Imery <ejwimery@gmail.com>
2019-08-09 08:49:16 -0600

    Creating Title component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Services extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return <div>Hello From services</div>;
+  }
 }
