c701a58353c81d5598864f5acc87d685f8680ba2
Eduardo Imery <ejwimery@gmail.com>
2019-08-09 09:04:52 -0600

    editing services component with hotel services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
 import Title from "../components/Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 
 export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktail",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, eius."
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless Hiking",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, eius."
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "free shuttle",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, eius."
+      },
+      {
+        icon: <FaBeer />,
+        title: "Strongest Beer",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, eius."
+      }
+    ]
+  };
   render() {
     return (
       <div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,22 +4,21 @@ import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import Services from "../components/Services";
 
-
 const home = () => {
   return (
-    //reactFragment 
+    //reactFragment
     <>
-    <Hero>
-      <Banner
-        title="Luxurious and confortable rooms"
-        subtitle="deluxe room starting at $199"
-      >
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
-    <Services/>
+      <Hero>
+        <Banner
+          title="Luxurious and confortable rooms"
+          subtitle="deluxe room starting at $199"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
     </>
   );
 };
