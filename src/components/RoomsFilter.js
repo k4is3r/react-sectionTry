eb47e326c37e861426d206af02b9b1542df973a9
Eduardo Imery <ejwimery@gmail.com>
2019-08-28 15:39:59 -0600

    modifiying roomfilter and context

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,5 +1,17 @@
 import React from "react";
+import {useContext} from 'react';
+import {RoomContext} from '../context';
+import Title from '../components/Title';
+
 
 export default function RoomsFilter() {
-  return <div>Hello from Rooms Filter</div>;
+  const context = useContext(RoomContext);
+  const {
+    handleChange, type, capacity, price, minPrice, masPrice, 
+    minSize, maxSize, breakfast, pets
+  } = context;
+
+  return <section className="filert-contai"> 
+    
+  </section>;
 }
