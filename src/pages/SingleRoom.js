3088a59271747f2c44073c8a4c0c9879031c7917
Eduardo Imery <ejwimery@gmail.com>
2019-08-28 19:00:58 -0600

    ending all filter features with room filter

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -64,7 +64,7 @@ export default class SingleRoom extends Component {
             <article className="info">
               <h3>infor</h3>
               <h6>price: ${price}</h6>
-              <h6>size: ${size} SQFT</h6>
+              <h6>size: {size} SQFT</h6>
               <h6> max capacity :{" "} {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
                 <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
                 <h6>{breakfast && "Free breakfast included"}</h6>
